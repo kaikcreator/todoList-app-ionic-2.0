549b47040568646a2c8c957da12e3332589cdb7a
-KaiK- <enriqueoriol@gmail.com>
2017-02-16 07:28:57 +0100

    14.1 - filtering with pipes

---
+++ src/pipes/prioritized-todos-pipe.ts
@@ -0,0 +1,21 @@
+import { Injectable, Pipe } from '@angular/core';
+import { TodoModel } from '../shared/todo-model';
+/*
+  Generated class for the PrioritizedTodosPipe pipe.
+
+  See https://angular.io/docs/ts/latest/guide/pipes.html for more info on
+  Angular 2 Pipes.
+*/
+@Pipe({
+  name: 'prioritizedTodosPipe',
+  pure: false
+})
+@Injectable()
+export class PrioritizedTodosPipe {
+  /*
+    Takes a value and makes it lowercase.
+   */
+  transform(todos: TodoModel[]) {
+    return todos.filter(todo => !todo.isDone).sort((a, b)=>(b.isImportant && !a.isImportant) ? 1 : -1);
+  }
+}
