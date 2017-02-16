e43253f0db34f5b5d8ec03d191d58e9da2f8d929
-KaiK- <enriqueoriol@gmail.com>
2017-02-16 07:28:57 +0100

    14.1 - filtering with pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { MyApp } from './app.component';
 import { TodosPage } from '../pages/todos/todos';
 import { AddTaskModalPage } from '../pages/add-task-modal/add-task-modal';
 import { TodoService } from '../shared/todo-service';
+import { PrioritizedTodosPipe } from '../pipes/prioritized-todos-pipe';
 
 @NgModule({
   declarations: [
     MyApp,
     TodosPage,
-    AddTaskModalPage
+    AddTaskModalPage,
+    PrioritizedTodosPipe
   ],
   imports: [
     IonicModule.forRoot(MyApp)
