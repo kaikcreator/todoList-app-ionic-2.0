74ac4060b559c2560d161c934495767c55783d14
-KaiK- <enriqueoriol@gmail.com>
2017-04-12 12:38:22 +0200

    udpated app to work with Ionic 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { IonicStorageModule } from '@ionic/storage';
@@ -20,6 +22,8 @@ import { DoneTodosPipe } from '../pipes/done-todos-pipe';
     DoneTodosPipe
   ],
   imports: [
+    BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
     IonicStorageModule.forRoot()
   ],
